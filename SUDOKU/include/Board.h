5a414be1246b80c2408ca675e3996a473e90c81b
Roman <73990909+Roma595@users.noreply.github.com>
2025-02-01 16:51:34 +0300

    init sudoku

---
+++ SUDOKU/include/Board.h
@@ -0,0 +1,22 @@
+#pragma once 
+
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <vector>
+
+class Board {
+
+public:
+    bool initialize_board();
+    bool is_valid_cell(int row, int col, int num);
+    bool check_board_is_full();
+    bool check_board_is_valid();
+    void generate_sudoku(int difficulty = 40);
+    int count_solutions();
+
+
+    int board[9][9] = {0};
+    
+
+};
---
+++ SUDOKU/src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Game.h"
+
+int main() {
+
+    Game game;
+    game.run();
+    // Board board;
+    // board.initialize_board();
+    // for (int i = 0; i < 9; i++) {
+    //     for (int j = 0; j < 9; j++) {
+    //         std::cout << board.board[i][j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+    // std::cout << std::endl;
+    // board.generate_sudoku();
+
+    // for (int i = 0; i < 9; i++) {
+    //     for (int j = 0; j < 9; j++) {
+    //         std::cout << board.board[i][j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+    return 0;
+}
