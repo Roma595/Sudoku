e702aa41ec969b5d8a36709c44c4e48ffdddace9
Roman <73990909+Roma595@users.noreply.github.com>
2025-02-05 19:01:49 +0300

    error_correction#1

--- SUDOKU/src/Board.cpp
+++ SUDOKU/src/Board.cpp
@@ -1,80 +1,133 @@
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <vector>
+
 #include "Board.h"
 
-std::vector<int> get_random_numbers() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
+namespace {
 
-    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::shuffle(numbers.begin(), numbers.end(), gen);
+    std::vector<int> get_random_numbers() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
 
-    return numbers;
+        std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        std::shuffle(numbers.begin(), numbers.end(), gen);
+
+        return numbers;
+    }
+
+    std::vector<Cell> get_cells() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        std::vector<Cell> cells;
+
+        for (size_t i = 0; i < 9; ++i){
+            for (size_t j = 0; j < 9; ++j){
+                cells.push_back(Cell{i,j});
+            }
+        }
+
+        std::shuffle(cells.begin(), cells.end(), gen);
+        return cells;
+    }
+
+
+
+} // namespace
+
+void Board::set_cell_in_board(size_t row, size_t col, const int num) {
+    board[row][col] = num;
 }
 
-std::vector<std::pair<int,int>> get_cells(){
-    std::random_device rd;
-    std::mt19937 gen(rd());
+int Board::get_cell_from_board(size_t row, size_t col) const {
+    return board[row][col];
+}
 
-    std::vector<std::pair<int,int>> cells;
+size_t Board::get_board_size() const {
+    return BOARD_SIZE;
+}
 
-    for (int i = 0; i < 9; i++){
-        for (int j = 0; j < 9; j++){
-            cells.push_back(std::make_pair(i, j));
+void Board::create_inaccessible_cells(){
+    for (size_t row = 0; row < 9; ++row){
+        for (size_t col = 0; col < 9; ++col){
+            if (board[row][col] != 0){
+                inaccessible_cells.push_back(Cell{row,col});
+            }
         }
     }
-    
-    std::shuffle(cells.begin(), cells.end(), gen);
-    return cells;
 }
 
+bool Board::is_cell_unavailable(size_t row, size_t col) const{
+    if(std::find(inaccessible_cells.begin(),inaccessible_cells.end(),Cell{row,col}) != inaccessible_cells.end()){
+        return true;
+    }
+    return false;
+}
+
+//переписать
 bool Board::initialize_board() {
     std::vector<int> numbers = get_random_numbers();
 
-    for (int row = 0; row < 9; row++) {
-        for (int col = 0; col < 9; col++) {
+    for (size_t row = 0; row < 9; ++row) {
+        for (size_t col = 0; col < 9; ++col) {
             if (board[row][col] == 0) {
                 for (int num : numbers) {
-                    if (is_valid_cell(row, col, num)) {
+                    if (is_digit_allowed(row, col, num)) {
                         board[row][col] = num;
                         if (initialize_board()) {
                             return true;
-                        } 
-                        else {
-                            board[row][col] = 0;  // backtrack
                         }
+                        board[row][col] = 0;  // backtrack
                     }
                 }
-                return false;  
-            }
-        }
-    }
-    return true; 
-}
-
-bool Board::is_valid_cell(int row, int col, int num) {
-    for ( int i = 0; i < 9; i++ ) {
-        // проверка что в строке и столбце нет такого же числа
-        if ( board[row][i] == num || board[i][col] == num) {
-            return false;
-        }
-    }
-
-    // проверка что в квадрате нет такого же числа
-    int start_row = 3 * (row / 3);   //0, 3, 6
-    int start_col = 3 * (col / 3);   //0, 3, 6
-
-    for ( int i = 0; i < 3; ++i){
-        for (int j = 0; j < 3; ++j){
-            if (board[start_row + i][start_col + j] == num){
                 return false;
             }
         }
+    }
+    return true;
+}
+
+bool Board::check_cell_in_row_and_col(const size_t row, const size_t col, const int num) {
+    for (size_t i = 0; i < 9; ++i) {
+        if(row == i || col == i){
+            continue;
+        }
+        if (board[row][i] == num || board[i][col] == num) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Board::check_cell_in_square(const size_t row, const size_t col, const int num) {
+    size_t square_first_row = 3 * (row / 3);
+    size_t square_first_col = 3 * (col / 3);
+
+    for (size_t square_row = 0; square_row < 3; ++square_row) {
+        for (size_t square_col = 0; square_col < 3; ++square_col) {
+            if(row == square_first_row + square_row && col == square_first_col + square_col){
+                continue;
+            }
+            if (board[square_first_row + square_row][square_first_col + square_col] == num) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool Board::is_digit_allowed(const size_t row, const size_t col, int num) {
+    if (!check_cell_in_row_and_col(row, col, num) || !check_cell_in_square(row, col, num)) {
+        return false;
     }
     return true;
 }
 
 bool Board::check_board_is_full() {
-    for (int row = 0; row < 9; row++) {
-        for (int col = 0; col < 9; col++) {
+    for (size_t row = 0; row < 9; ++row) {
+        for (size_t col = 0; col < 9; ++col) {
             if (board[row][col] == 0) {
                 return false;
             }
@@ -83,17 +136,11 @@ bool Board::check_board_is_full() {
     return true;
 }
 
-bool Board::check_board_is_valid() {
-    for (int row = 0; row < 9; row++) {
-        for (int col = 0; col < 9; col++) {
-            if (board[row][col] != 0) {
-                int num = board[row][col];
-                board[row][col] = 0;
-                if (!is_valid_cell(row, col, num)) {
-                    board[row][col] = num;
-                    return false;
-                }
-                board[row][col] = num;
+bool Board::is_valid_board() {
+    for (size_t row = 0; row < 9; ++row) {
+        for (size_t col = 0; col < 9; ++col) {
+            if (!is_digit_allowed(row, col, board[row][col])){
+                return false;
             }
         }
     }
@@ -104,14 +151,14 @@ int Board::count_solutions() {
     int count = 0;
     std::vector<int> numbers = get_random_numbers();
 
-    for (int row = 0; row < 9; row++) {
-        for (int col = 0; col < 9; col++) {
+    for (size_t row = 0; row < 9; ++row) {
+        for (size_t col = 0; col < 9; ++col) {
             if (board[row][col] == 0) {
                 for (int num : numbers) {
-                    if (is_valid_cell(row, col, num)) {
+                    if (is_digit_allowed(row, col, num)) {
                         board[row][col] = num;
                         count += count_solutions();
-                        board[row][col] = 0;  // backtrack
+                        board[row][col] = 0;
                     }
                 }
                 return count;
@@ -121,23 +168,21 @@ int Board::count_solutions() {
     return 1;
 }
 
-void Board::generate_sudoku(int difficulty) {
-    std::vector<std::pair<int,int>> cells = get_cells();
+void Board::generate_sudoku(int digits_to_remove) {
+    std::vector<Cell> cells = get_cells();
     for (auto& cell : cells){
-        if (difficulty == 0){
+        if (digits_to_remove == 0) {
             break;
         }
-        int row = cell.first;
-        int col = cell.second;
-        int temp = board[row][col];
-        board[row][col] = 0;
-        
-        if (count_solutions() != 1){
-            board[row][col] = temp;
+        int temp = board[cell.row][cell.col];
+        board[cell.row][cell.col] = 0;
+
+        if (count_solutions() != 1) {
+            board[cell.row][cell.col] = temp;
         }
 
-        else{
-            difficulty--;
+        else {
+            digits_to_remove--;
         }
     }
 }
